912bcc140c048827dbe88aa339d440447b6642fc
ybw0014 <i@guizhanss.com>
2025-05-23 12:49:59 -0400

    chore: fix build

--- guizhanlib-kt-slimefun/build.gradle.kts
+++ guizhanlib-kt-slimefun/build.gradle.kts
@@ -10,5 +10,5 @@ dependencies {
     api(project(":guizhanlib-kt-minecraft", configuration = "shadow"))
     compileOnlyAndTestImpl("net.guizhanss:guizhanlib-slimefun:$guizhanLibVersion")
     compileOnlyAndTestImpl("io.papermc.paper:paper-api:1.21.3-R0.1-SNAPSHOT")
-    compileOnlyAndTestImpl("com.github.slimefun:Slimefun4:3ea21da4fe")
+    compileOnlyAndTestImpl("com.github.slimefun:Slimefun4:experimental-SNAPSHOT")
 }
