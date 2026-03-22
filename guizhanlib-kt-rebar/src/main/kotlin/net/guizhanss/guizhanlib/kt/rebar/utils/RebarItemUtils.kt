288ce9280aa2140eb4890151684441d183c306d5
ybw0014 <i@guizhanss.com>
2026-03-22 18:45:56 -0400

    feat: persistentItemData can accept direct value

--- guizhanlib-kt-rebar/src/main/kotlin/net/guizhanss/guizhanlib/kt/rebar/utils/RebarItemUtils.kt
+++ guizhanlib-kt-rebar/src/main/kotlin/net/guizhanss/guizhanlib/kt/rebar/utils/RebarItemUtils.kt
@@ -1,11 +1,16 @@
 package net.guizhanss.guizhanlib.kt.rebar.utils
 
+import io.github.pylonmc.rebar.config.adapter.ConfigAdapter
 import io.github.pylonmc.rebar.item.RebarItem
 import org.bukkit.NamespacedKey
 import org.bukkit.persistence.PersistentDataType
+import kotlin.properties.ReadOnlyProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
+/**
+ * Property delegation to read/set value in the bound stack's PersistentDataContainer.
+ */
 @JvmSynthetic
 inline fun <reified T : RebarItem, V> T.persistentItemData(
     key: NamespacedKey,
@@ -24,3 +29,13 @@ inline fun <reified T : RebarItem, V> T.persistentItemData(
         }
     }
 }
+
+/**
+ * Property delegation to read/set value in the bound stack's PersistentDataContainer.
+ */
+@JvmSynthetic
+inline fun <reified T : RebarItem, V> T.persistentItemData(
+    key: NamespacedKey,
+    type: PersistentDataType<*, V & Any>,
+    default: V
+) = persistentItemData(key, type) { default }
